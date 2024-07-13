96ae7e97f1921796377d34a2697e501965ee8aef
kapilsarkar <k.sarkar81@gmail.com>
2024-07-13 17:06:04 +0530

    initialcommit

---
+++ DAY-01-VARIABLES AND DATATYPES/index.js
@@ -0,0 +1,24 @@
+//Activity-1 : Variable Declaration
+
+//Task-1 : Declare a variable using var, assign it a number and log the value to the console
+
+var number = 20
+console.log(number)
+
+//Task-2 : Declare a variable using let, assign it a string and log the value to the console
+
+let myName = 'Kapil Sarkar'
+console.log(myName)
+
+//Activity-2 : Constant Declaration
+
+//Task-3 : Declare a variable using const, assign it a boolean value and log the value to the console
+
+const isLogged = true
+console.log(isLogged)
+
+
+
+
+
+
