2c44238f69db345b7954e2b9c8f6eeccb9908ff4
kapilsarkar <k.sarkar81@gmail.com>
2024-07-14 07:47:12 +0530

    day01completed

--- DAY-01-VARIABLES AND DATATYPES/index.js
+++ DAY-01-VARIABLES AND DATATYPES/index.js
@@ -17,6 +17,50 @@ console.log(myName)
 const isLogged = true
 console.log(isLogged)
 
+//Activity-3 : Data Types 
+
+//Task-4 : Create variables of different data types (number,string boolean,object,array) and log each variable's  type using the typeof operator.
+
+const aNumber = 32
+console.log(typeof aNumber)
+
+const myStar = "John Cena"
+console.log(typeof myStar)
+
+const isMember = true
+console.log(typeof isMember)
+
+const myDetails ={
+    myname : "Kapil",
+    role: "Frontend Developer",
+}
+
+console.log(typeof myDetails)
+
+const arrayOfStars = ["Hrithik","Kohli","Rohit Sharma"]
+console.log(typeof arrayOfStars)
+
+
+//Activity-4 : Reassigning variables
+
+//Task-5 : Declare a variable using let, assign it an initial value,reassign a new value,and log both values to the console
+
+let myFavCricketer = "Sachin Tendulkar"
+console.log(myFavCricketer)
+
+myFavCricketer = "Virat Kohli"
+console.log(myFavCricketer)
+
+//Activity-5 : Understanding const
+
+//Task-6 : Try reassigning a variable declared with const and observer the error.
+
+const myFavActor = "Akshay Kumar"
+console.log(myFavActor)
+
+myFavActor = "Hritik Roashan"
+console.log(myFavActor)
+
 
 
 
