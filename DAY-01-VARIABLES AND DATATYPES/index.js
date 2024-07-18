fe86e3f38462b4c8ebb9a90f20e9dd39d808f7e2
kapilsarkar <k.sarkar81@gmail.com>
2024-07-18 07:26:38 +0530

    completed

--- DAY-01-VARIABLES AND DATATYPES/index.js
+++ DAY-01-VARIABLES AND DATATYPES/index.js
@@ -55,11 +55,63 @@ console.log(myFavCricketer)
 
 //Task-6 : Try reassigning a variable declared with const and observer the error.
 
-const myFavActor = "Akshay Kumar"
-console.log(myFavActor)
+// const myFavActor = "Akshay Kumar"
+// console.log(myFavActor)
 
-myFavActor = "Hritik Roashan"
-console.log(myFavActor)
+// myFavActor = "Hritik Roashan"
+// console.log(myFavActor)
+
+// ⚠️ Error Observed - TypeError: Assignment to constant variable.
+
+// Feature Request :
+
+//1. Variable Types Console Log: Write a script that declares variables of different data types and logs both the values of each variable to the console.
+
+let stringValue = "My Name is Kapil Sarkar"
+console.log(stringValue , "Data Type is :", typeof stringValue)
+
+let numValue = 32
+console.log(numValue,"Data Type is :",typeof numValue)
+
+let isPassed = true
+console.log(isPassed,"Data Type is :",typeof isPassed)
+
+let Cricketers = ["Sachin","Rohit","Kohli","Bumrah","Zaheer"]
+console.log(Cricketers,"Data Tpe is :",typeof Cricketers)
+
+let myDetails2 = {
+      name:"Kapil Sarkar",
+      role :"front-End Developer",
+      education:"MCA"
+}
+console.log(myDetails2,"Data Type is :",typeof myDetails2)
+
+let value = null
+console.log(value,"Data Type is:",typeof value)
+
+let isTrue = undefined
+console.log(isTrue,"Data Type is :",typeof isTrue)
+
+// 2. Reassignment Demo: Create a script that demonstrates the difference in behavior between `let` and `const` when it comes to reassignment. 
+
+//Reassignment of let variable :
+let Winner2007WorldCup = "INDIA"
+console.log("Initial Value of let:",Winner2007WorldCup)
+
+Winner2007WorldCup = "AUSTRALIA"
+console.log("New Value of let:",Winner2007WorldCup)
+
+//Reassignment of const variable :
+const firstValueofConst = "REACT-JS"
+console.log(firstValueofConst)
+
+secondValueofConst = "ANGULAR-JS"
+console.log(secondValueofConst)
+
+
+
+
+
 
 
 
