b479156a61e1d08047d16674edd3529298e25490
kapilsarkar <k.sarkar81@gmail.com>
2024-07-18 07:53:21 +0530

    somepartsadded

---
+++ DAY-02-OPERATORS/index.js
@@ -0,0 +1,37 @@
+//Activity-1: Arithmetic Operators
+
+
+//Task-1 : Write a program to add two numbers and log the result to the console.
+let addNum1 = 10
+let addNum2 = 20
+let addResult = addNum1 + addNum2
+console.log("Result of Addition is :", addResult)
+
+//Task-2 : Write a program to subtract two numbers and log the result to the console.
+let subNum1 = 20
+let subNum2 = 10
+let subResult = subNum1 - subNum2
+console.log("Result of Subtraction is :", subResult)
+
+//Task-3 : Write a program to multiply two numbers and log the result to the console.
+let mulNum1 = 30
+let mulNum2 = 20
+let mulResult = mulNum1 * mulNum2
+console.log("Result of Multiplication is :", mulResult)
+
+//Task-4 : Write a program to divide two numbers and log the result to the console.
+let divNum1 = 50
+let divNum2 = 10
+console.log("Result of Division L :",divNum1 / divNum2)
+
+//Task-5 : Write a program to find the remainder when one number is divided by another  and log the result to the console.
+
+let divRemNum1 = 54
+let divRemNum2 = 9
+let divRemResult = divRemNum1 % divRemNum2
+console.log("Result of  Division of and Remainder is ",divRemResult)
+
+
+
+
+
