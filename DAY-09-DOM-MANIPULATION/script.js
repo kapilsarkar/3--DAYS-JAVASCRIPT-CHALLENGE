cb71c721c2124bfe207733ff0b9d9b31dce1750c
kapilsarkar <k.sarkar81@gmail.com>
2024-07-24 16:28:39 +0530

    somepartsadded

---
+++ DAY-09-DOM-MANIPULATION/script.js
@@ -0,0 +1,6 @@
+//Activity-1 : Selecting and Manipulating Elements
+
+//Task-1 : Select an HTML element by its ID and change its context.
+
+const tas1Id = document.getElementById("tas1Id")
+tas1Id.innerText = ""
